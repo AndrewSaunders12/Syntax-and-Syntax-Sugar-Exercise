a088330aa51687c99eaceae6d03b52119cb8b927
AndrewSaunders12 <ninjaraider04@gmail.com>
2024-10-17 17:50:23 -0400

    Syntax sugar

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 8;
+
+            Console.WriteLine(answer < 9 ? $"{answer} is less then 9" : $"{answer} Is greater than or equal to 9" );
         }
     }
 }
